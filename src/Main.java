e252284b127287bd9cac2145038b76805b08fc60
gustavkjellberg <gustav.kjellberg@hotmail.com>
2018-09-17 13:38:42 +0200

    Hmm3

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,6 @@ public class Main {
         double[][] emissionMatrix = parse.emissionMatrix();
         double[][] initialStateVector = parse.initialStateVector();
         int[] emissions = parse.getEmissions();
-
-        /*double [][] first = (calcPropX(initialStateVector,transistionMatrix));
-        printMatrix(calcPropX(first, emissionMatrix));*/
-
-        //double[][] finalAlpha = observationLikelihood(transistionMatrix, emissionMatrix, emissions,initialStateVector, emissions.length-1);
-
-        int[] path = stateLiklihood(transistionMatrix, emissionMatrix, emissions, initialStateVector, emissions.length-1);
         parse.closeStream();
 
     }
@@ -69,14 +62,23 @@ public class Main {
 
     }
 
-
-    public static double[][] observationLikelihood(double[][] A, double[][] B, int[] obs, double[][] pi, int n){
-        if (n == 0){
-            return elementWiseMultiplication(pi,getColumn(B, obs[n]));
+    public static double[][] insertColInMatrix(double[][] col, double[][]matrix, int colIndex){
+        for (int index = 0; index<col.length;index++){
+            matrix[index][colIndex] = col[index][0];
         }
-        double[][] newAlpha = matrixMultiplication(observationLikelihood(A,B,obs,pi,n-1), A);
-        return elementWiseMultiplication(newAlpha, getColumn(B,obs[n]));
-
+        return matrix;
+    }
+    
+    public static double[][] alphaPass(double[][] A, double[][] B, int[] obs, double[][] pi, int n, double [][] alphaMatrix){
+        if (n == 0){
+            double[][] firstAlpha =  elementWiseMultiplication(pi,getColumn(B, obs[n]));
+            alphaMatrix = insertColInMatrix(firstAlpha, alphaMatrix, n);
+            return alphaMatrix;
+        }
+        double[][] newAlpha = matrixMultiplication(alphaPass(A,B,obs,pi,n-1, alphaMatrix), A);
+        double [][] alphaCol =  elementWiseMultiplication(newAlpha, getColumn(B,obs[n]));
+        alphaMatrix = insertColInMatrix(alphaCol, alphaMatrix, n);
+        return alphaMatrix;
     }
 
     public static double[][] getColumn(double[][] matrix, int n){
@@ -103,6 +105,14 @@ public class Main {
         return resultVector;
     }
 
+    public static double[][] scalarMultiplication(double scalar, double[][] column){
+        double[][] resultVector = new double[column.length][1];
+        for (int i = 0; i<column.length; i++){
+            resultVector[i][0] = scalar * column[i][0];
+        }
+        return resultVector;
+    }
+
     public static int[] stateLiklihood(double[][] A, double[][] B, int[] obs, double[][] pi, int n){
 
         double[][] delta = new double[A.length][n+1];
@@ -115,29 +125,65 @@ public class Main {
         }
 
 
-        for (int t = 1; t<n; t++){
+        for (int t = 1; t<=n; t++){
             for (int s = 0; s < A.length; s++){
-                double[][] res = (elementWiseMultiplicationCol(elementWiseMultiplicationCol(getColumn(delta, t-1), getColumn(A,s)), getColumn(B, obs[t])));
-                delta[s][t] = Arrays.stream(res[0]).max().getAsDouble();
-                double mini = 0.0;
-                for (int index = 0; index < res.length; index++){
-                    if(res[index][0]>mini){
-                        mini = res[index][0];
+                double[][] deltaAlpha =(elementWiseMultiplicationCol(getColumn(delta, t-1), getColumn(A,s)));
+                //double [][] res = elementWiseMultiplicationCol(deltaAlpha, getColumn(B, obs[t]));
+                double maxOfDeltaAlpha = 0.0;
+                for (int index = 0; index < deltaAlpha.length; index++){
+                    double potentiallyBigger = deltaAlpha[index][0];
+                    if (maxOfDeltaAlpha<potentiallyBigger){
+                        maxOfDeltaAlpha = potentiallyBigger;
                         phi[s][t] = index;
                     }
                 }
+
+                delta[s][t] = maxOfDeltaAlpha * B[s][obs[t]];
+
+                /*double [][] res = scalarMultiplication(maxOfDeltaAlpha, getColumn(B, obs[t]));
+                for (int index = 0; index<res.length; index++){
+                    delta[index][t] = res[index][0];
+                } */
+
+               /*double mini = 0.0;
+                for (int index = 0; index < deltaAlpha.length; index++){
+                    if(deltaAlpha[index][0]>mini){
+                        mini = deltaAlpha[index][0];
+                        phi[s][t] = index;
+                    }
+                }*/
             }
         }
 
-        int finalState = phi[A.length-1][n];
+        int finalState = 0;
+        double minVal = 0.0;
+        for (int index = 0 ; index <delta.length; index++){
+            if (delta[index][n] > minVal) {
+                finalState = index;
+            }
+        }
 
         int[] path = new int[n+1];
         path[n] = finalState;
         for (int t = n-1; t>=0;t--){
-            path[t] = phi[path[t+1]][t];
+            path[t] = phi[path[t+1]][t+1];
         }
 
         return path;
+    }
+
+    public static double[][] betaPass(double[][] A, double[][] B, int[] obs, int n, double[][] betaMatrix){
+        if (n == obs.length-1){
+            double[][] finalCol = new double[A.length][1];
+            for (int i = 0; i<finalCol.length; i++){
+                finalCol[i][0] = 1.0;
+            }
+            
+
+        }
+
+
+        return null;
     }
 
 }
